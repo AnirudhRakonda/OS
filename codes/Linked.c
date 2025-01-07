ca1174dded5f15bcaff11d5e1f2dfc91256b836e
AnirudhRakonda <140954132+AnirudhRakonda@users.noreply.github.com>
2025-01-07 21:34:53 +0530

    file-alloc

--- codes/Linked.c
+++ codes/Linked.c
@@ -24,9 +24,9 @@ int main() {
     for (int i = 0; i < n; i++) {
         printf("%d\t%d\t%d\t", i + 1, start[i], size[i]);
 
-        printf("%d --> " ,start[i]);
+        printf("%d , " ,start[i]);
         for (int j = 0; j < size[i]; j++) {
-            printf("%d --> ", index[i][j]);
+            printf("%d , ", index[i][j]);
         }
         printf("NULL\n"); 
     }
